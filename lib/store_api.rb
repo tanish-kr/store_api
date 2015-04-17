d80bc8eb2dac66bc2d16e709470bb107e6e3f644
tatsunori_nishikori <tatsunori_nishikori@metaps.com>
2015-04-17 17:08:24 +0900

    added http request module

--- lib/store_api.rb
+++ lib/store_api.rb
@@ -1,4 +1,9 @@
+# encoding : utf-8
+require "net/https"
+require "uri"
+require "nokogiri"
 require "store_api/version"
+require "store_api/request"
 
 module StoreApi
   # Your code goes here...
---
+++ lib/store_api/request.rb
@@ -0,0 +1,77 @@
+# encoding : utf-8
+module StoreApi
+
+  module Request
+
+    TIME_OUT = 20
+    @@redirect_count = 0
+
+    ##
+    # http get
+    # @params [String] host
+    # @params [String] path
+    # @params [Hash] proxy [:host=>'host',:port=>port]
+    # @params [Hash] params
+    # @return [String] response.body
+    def get(host,path,https=false,proxy=nil,option=nil,params=nil)
+      request(host,path,'get',https,proxy,option,params).body
+    end
+
+    ##
+    # http request
+    # @params [String] host
+    # @params [String] path
+    # @params [Hash] proxy [:host=>'host',:port=>port]
+    # @params [String] request get/post
+    # @params [Hash] params
+    # @return [Object] response
+    def request(host,path,rest='get',https=false,proxy=nil,option=nil,params=nil)
+      if !proxy.nil? && !proxy.empty?
+        if https
+          http = Net::HTTP::Proxy(proxy[:host],proxy[:port]).new(host,443)
+        else
+          http = Net::HTTP::Proxy(proxy[:host],proxy[:port]).new(host)
+        end
+      else
+        if https
+          http = Net::HTTP.new(host,443)
+          http.use_ssl = true
+        else
+          http = Net::HTTP.new(host)
+        end
+      end
+      http.read_timeout = TIME_OUT
+      if !option.nil? && !option.empty?
+        if request == 'get'
+          response = http.get(path,option)
+          #TODO post/put
+        end
+      else
+        if rest == 'get'
+          response = http.get(path)
+          #TODO post/put
+        end
+      end
+      if response.code.to_i > 500
+        @@redirect_count = 0
+        raise "Server Error ! responce code = #{response.code} response = #{response.body}"
+      elsif response.code.to_i >= 400 && response.code.to_i < 500
+        @@redirect_count = 0
+        raise "Not Found ! responce codee = #{response.code} response = #{response.body}"
+      elsif response.code.to_i >= 300 && response.code.to_i < 400
+        redirect_url = URI.parse(response.header['location'])
+        if redirect_url.scheme == "https"
+          https = true
+        end
+        @@redirect_count = @@redirect_count + 1
+        if @@redirect_count > 5
+          raise "Exception Redirect Loop"
+        end
+        response = request(redirect_url.host,redirect_url.path,rest,https,proxy,option,params)
+      else
+        @redirect_count = 0
+        response
+      end
+    end
+  end
+end
