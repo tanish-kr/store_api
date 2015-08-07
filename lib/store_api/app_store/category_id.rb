c525d5196c459d4d1097ce0fd012b5bcc646ad29
tatsu <tora.1986.tatsu@gmail.com>
2015-08-07 16:48:31 +0900

    added ranking type and category id

---
+++ lib/store_api/app_store/category_id.rb
@@ -0,0 +1,26 @@
+# encoding : utf-8
+module StoreApi
+  module AppStore
+    CATEGORY_ID_LIST = [
+      '6018', # Books
+      '6000', # Business
+      '6022', # Catalogs
+      '6017', # Education
+      '6016', # Entertainment
+      '6015', # Finance
+      '6023', # Food & Drink
+      '6014', # Games
+      '6013', # Health & Fitness
+      '6012', # Lifestyle
+      '6020', # Medical
+      '6011', # Music
+      '6010', # Navigation
+      '6009', # News
+      '6021', # Newsstand
+      '6008', # Photo & Video
+      '6007', # Productivity
+      '6006', # Reference
+      '6005'  # Social Networking
+    ]
+  end
+end
---
+++ lib/store_api/app_store/ranking_type.rb
@@ -0,0 +1,16 @@
+# encoding : utf-8
+module StoreApi
+  module AppStore
+    RANKING_TYPE_LIST = [
+      'topgrossingapplications', # Top Grossing Applications
+      'topgrossingipadapplications', # Top Grossing iPad Applications
+      'newapplications', # New Applications
+      'newfreeapplications', # New Free Applications
+      'newpaidapplications', # New Paid Applications
+      'topfreeapplications', # Top Free Applications
+      'topfreeipadapplications', # Top Free iPad Applications
+      'toppaidapplications', # Top Paid Applications
+      'toppaidipadapplications' # Top Paid iPad Applications
+    ]
+  end
+end
